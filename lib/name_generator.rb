5693a34c95129d6ede295fb9d1a24947ed51bbca
Egor Gromyko <g.gromyko@icloud.com>
2015-06-07 19:40:13 +0500

    add the NameGenerator class

---
+++ lib/name_generator.rb
@@ -0,0 +1,45 @@
+class NameGenerator
+  def generate
+    generate_name
+  end
+
+  private
+
+  def generate_syllable
+
+    consonants = %w{ b c d f g h j k l m n p r s t v w x z }
+    vowels = %w{ a o e u i }
+
+    letters = []
+    letters << consonants.sample
+    letters << vowels.sample
+    letters << consonants.sample if rand < 0.5
+
+    # letters.shuffle.join
+    letters.join
+  end
+
+  def generate_string
+
+    syllables = []
+
+    10.times do
+      syllables << generate_syllable
+    end
+
+    syllables.shuffle.join
+  end
+
+  def generate_name
+    string = generate_string
+    random_start = rand(3)
+    random_end = rand(random_start + 3..10)
+    string[random_start..random_end].capitalize
+  end
+end
+
+generator = NameGenerator.new
+
+name = generator.generate
+
+puts name
---
+++ test/test_name_generator.rb
@@ -0,0 +1,21 @@
+require 'test/unit'
+require_relative '../lib/name_generator.rb'
+
+class TestNameGenerator < Test::Unit::TestCase
+  def setup
+    @generator = NameGenerator.new
+    @name = @generator.generate
+  end
+
+  def test_that_returns_valid_name
+    assert_instance_of String, @name, "Should be a string"
+    assert @name =~ /^[A-Z][a-z]+$/, "Name should contain valid symbols"
+    assert (@name.length >= 3 && @name.length <= 12), "Name length should be between 3 and 12 symbols"
+    assert_nil @name =~ /[BCDFGHJKLMNPQRSTVWXZbcdfghjklmnpqrstvwxz]{3,}/, "Should contain no more than 2 consonants in a row"
+
+  end
+
+  def test_name_is_random
+    refute_equal @name, @generator.generate
+  end
+end
